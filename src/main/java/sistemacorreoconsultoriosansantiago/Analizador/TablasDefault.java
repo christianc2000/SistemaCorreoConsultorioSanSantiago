87ecc0ac251a7338479a3cf51026dabaf1587cca
lizreina1 <lizreina.rq@gmail.com>
2023-12-05 09:16:18 -0400

    turno

--- src/main/java/sistemacorreoconsultoriosansantiago/Analizador/TablasDefault.java
+++ src/main/java/sistemacorreoconsultoriosansantiago/Analizador/TablasDefault.java
@@ -19,11 +19,16 @@ public class TablasDefault {
         List<String> usuarioAtr = Arrays.asList("id", "ci", "nombre", "apellido", "fechaNacimiento", "celular", "tipo", "genero", "residenciaActual", "email", "password", "urlFoto", "formacion", "sueldo", "hijo", "ocupacion");
         List<String> consultaAtr = Arrays.asList("id", "fecha", "motivo", "examenFisico", "observaciones", "diagnostico", "historialId");
         List<String> reporteAtr = Arrays.asList("id", "fechaReporte", "urlDocumento", "userId");
+        List<String> turnoAtr = Arrays.asList("id", "dia", "horaInicio", "horaFin");
+        List<String> servicioAtr = Arrays.asList("id", "nombre", "costo", "formaCompra","atencion");
 
         Tabla tablaUsuario = new Tabla("users", usuarioAtr);
         Tabla tablaConsulta = new Tabla("consultas", consultaAtr);
         Tabla tablaReporte = new Tabla("reportes", reporteAtr);
-        this.tablas = new Tabla[]{tablaUsuario, tablaConsulta, tablaReporte};
+        Tabla tablaTurno = new Tabla("turnos", turnoAtr);
+        Tabla tablaServicio = new Tabla("servicios", servicioAtr);
+        
+        this.tablas = new Tabla[]{tablaUsuario, tablaConsulta, tablaReporte,tablaTurno,tablaServicio};
     }
 
     public Tabla[] getTablas() {
